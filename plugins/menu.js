b9a3c9df03f721d922c922d4c2a57ad9cbabe17e
𝙄𝙢𝙤𝙓𝙯𝙯𝙮 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 <erenxz01@gmail.com>
2025-09-09 16:33:24 -0400

    Actualizar menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -45,7 +45,7 @@ const menuCommand = {
 
     for (const category of sortedCategories) {
       const emoji = categoryEmojis[category] || categoryEmojis['default'];
-      menuText += `╭─〔 ${emoji} ${category.toUpperCase()} 〕\n`;
+      menuText += `╭━━━〔 ${emoji} ${category.toUpperCase()} 〕\n`;
 
       const commandList = categories[category]
         .filter((cmd, index, self) => self.findIndex(c => c.name === cmd.name) === index) // evitar duplicados
@@ -53,7 +53,7 @@ const menuCommand = {
         .join('\n');
 
       menuText += `${commandList}\n`;
-      menuText += `╰━━━━━━━━━━━━━━━━━╯\n\n`;
+      menuText += `╰━━━━━━━━━━━━━━━━━━╯\n\n`;
     }
 
     await sock.sendMessage(
