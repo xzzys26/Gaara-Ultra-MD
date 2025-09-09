b837663d58f5cdb1d32f898c383c5f373c42e275
𝙄𝙢𝙤𝙓𝙯𝙯𝙮 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 <erenxz01@gmail.com>
2025-09-09 16:37:51 -0400

    Actualizar menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -38,14 +38,14 @@ const menuCommand = {
 
     // --- Construcción del menú con tu decoración ---
     let menuText = `╭━━━〔 *${config.botName}* 〕━━━╮\n`;
-    menuText += `┃ ✎ Hola: *${msg.pushName}*\n`;
-    menuText += `┃ ✎ Versión: *${config.version || '1.0.0'}*\n`;
-    menuText += `┃ ✎ Owner: *${config.ownerName}*\n`;
+    menuText += `┃ ➪ 𝗛𝗼𝗹𝗮 𝗨𝘀𝗲𝗿: *${msg.pushName}*\n`;
+    menuText += `┃ ➪ 𝗩𝗲𝗿𝘀𝗶𝗼𝗻 𝗗𝗲𝗹 𝗕𝗼𝘁: *${config.version || '1.0.0'}*\n`;
+    menuText += `┃ ➪ 𝗢𝘄𝗻𝗲𝗿: *${config.ownerName}*\n`;
     menuText += `╰━━━━━━━━━━━━━━━━━━━━━━━╯\n\n`;
 
     for (const category of sortedCategories) {
       const emoji = categoryEmojis[category] || categoryEmojis['default'];
-      menuText += `╭─〔 ${emoji} ${category.toUpperCase()} 〕\n`;
+      menuText += `╭━━━〔 ${emoji} ${category.toUpperCase()} 〕\n`;
 
       const commandList = categories[category]
         .filter((cmd, index, self) => self.findIndex(c => c.name === cmd.name) === index) // evitar duplicados
@@ -53,13 +53,13 @@ const menuCommand = {
         .join('\n');
 
       menuText += `${commandList}\n`;
-      menuText += `╰━━━━━━━━━━━━━━━━━━━━━━━╯\n\n`;
+      menuText += `╰━━━━━━━━━━━━━━━━━━╯\n\n`;
     }
 
     await sock.sendMessage(
       msg.key.remoteJid,
       {
-        image: { url: 'https://files.catbox.moe/itgz1x.png' },
+        image: { url: 'https://russellxz.click/c02f8044.png' },
         caption: menuText,
         mimetype: 'image/png'
       },
