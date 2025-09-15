8015a59d76f5218a41dfc392a878132c45630ea7
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-15 14:59:12 -0700

    Delete plugins directory

--- plugins/yt.js
+++
@@ -1,9 +0,0 @@
-import playCommand from './play.js';
-
-const ytCommand = {
-  ...playCommand, // Copia toda la funcionalidad de play.js
-  name: "yt", // Pero con un nombre diferente
-  description: "Alias para el comando `play`." // Y una descripción diferente
-};
-
-export default ytCommand;
