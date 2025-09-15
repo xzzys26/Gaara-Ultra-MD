bd7bf35faa9e2d335af87e9ac15f1fca3cba227a
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-15 12:44:43 -0700

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,25 +1,105 @@
-const config = {
-  botName: "𝗚𝗔𝗔𝗥𝗔 𝗨𝗟𝗧𝗥𝗔-𝗠𝗗",
-  ownerName: "xzzys26",
-  taxRate: 0.10,
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
 
-  ownerNumbers: [
-    "1920437612698",
-    "18493907272",
-    "101524940267597",
-    "1920437612698",
-    "", 
-    "", 
-    ""
-  ],
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-  api: {
-    ytmp3: "https://myapiadonix.vercel.app/api/ytmp3",
-    ytmp4: "https://myapiadonix.vercel.app/api/ytmp4",
-    gemini: "AIzaSyDEww4IKqba9tgfb8ndMDBOoLkl-nSy4tw"
-  },
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+global.botNumber = '' //Ejemplo: 573218138672
 
-  authDir: "Gaara-Ultra Session"
-};
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-export default config;
+global.creador = [
+  ['18493907272', '☆ 𝐶𝑟𝑒𝑎𝑑𝑜𝑟 ☆', true],
+]
+
+global.owner = [
+// <-- Número @s.whatsapp.net -->
+  ['18493907272', '☆ 𝐶𝑟𝑒𝑎𝑑𝑜𝑟 ☆', true],
+  ['50432955554'],
+  ['51927238856'],
+  ['584146277368'], 
+  ['5216671548329'], 
+  ['5212431268546'],
+  ['573143681055'],
+  ['90263687053350@lid'],
+
+// <-- Número @lid -->
+//  ['', 'BrayanOFC, true],
+  ['258892692984006', 'DevAlexJs', true], 
+];
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = []
+global.suittag = ['5216641784469'] 
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.libreria = 'Baileys'
+global.baileys = 'V 6.7.17'  
+global.languaje = 'Español'
+global.vs = '2.13.2'
+global.vsJB = '5.0'
+global.nameqr = '𝚅𝙴𝙶𝙴𝚃𝙰 - 𝙱𝙾𝚃 - 𝙼𝙱'
+global.namebot = '✿◟𝚅𝚎𝚐𝚎𝚝𝚊-𝙱𝚘𝚝-𝙼𝙱◞✿'
+global.vegetasessions = 'vegetaSessions'
+global.jadi = 'JadiBots' 
+global.vegetaJadibts = true
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = `⪛✰¨♱𝚅𝙴𝙶𝙴𝚃𝙰-𝙱𝙾𝚃-𝙼𝙱`
+global.botname = '*♱𝚅𝙴𝙶𝙴𝚃𝙰-𝙱𝙾𝚃-𝙼𝙱♱*'
+global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ  ➳𝐁𝐫𝐚𝐲𝐚𝐧𝐎𝐅𝐂ღ'
+global.textbot = ' ➳𝐁𝐫𝐚𝐲𝐚𝐧𝐎𝐅𝐂❦ • P·:*¨♱𝚅𝙴𝙶𝙴𝚃𝙰-𝙱𝙾𝚃-𝙼𝙱♱ ¨*:·'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.moneda = 'dragones'
+global.welcom1 = 'Edita Con #setwelcome'
+global.welcom2 = 'Edita Con #setbye'
+global.banner = 'https://files.catbox.moe/j0z1kz.jpg'
+global.catalogo = 'https://files.catbox.moe/j0z1kz.jpg'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒──ׄ─ׅ─ׄ─
+
+global.gp1 = 'https://chat.whatsapp.com/HaKf6ezcwdbGzmH782eBal?mode=r_c'
+global.comunidad1 = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
+global.channel = 'https://whatsapp.com/channel/0029Vb64nWqLo4hb8cuxe23n'
+global.channel2 = 'https://whatsapp.com/channel/0029VbAfPu9BqbrEMFWXKE0d'
+global.md = 'https://githun.com/BrayanOFC/VEGETA-BOT-MB.git'
+global.correo = 'brayanofc70@gmail.com'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+*/global.catalogo = fs.readFileSync('./src/catalogo.jpg');*/
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.ch = {
+ch1: '120363394965381607@newsletter',
+ch2: "120363394965381607@newsletter",
+ch3: "120363394965381607@newsletter"
+}
+global.multiplier = 60
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+global.moment = moment   
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
