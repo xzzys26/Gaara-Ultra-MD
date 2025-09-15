e23a3067d5d646610a67d404d005482cd7c4bac0
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-15 16:14:36 -0700

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -249,10 +249,9 @@ if (plugin.tags && plugin.tags.includes('admin')) {
 continue
 }
 
-global.regexPrefix = /^[#!./]/i
+const str2Regex = str => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
 //  LÓGICA DE PREFIJOS PERSONALIZADOS AÑADIDA NO TOCAR PORFA SOLO BRAYANOFC.
-let _prefix = (plugin.customPrefix ? [plugin.customPrefix] : [])
-  .concat(global.db.data.settings[this.user.jid]?.prefix || global.regexPrefix);
+let _prefix = (plugin.customPrefix ? [plugin.customPrefix] : []).concat(global.db.data.settings[this.user.jid]?.prefix || global.prefix);
 let match = (_prefix instanceof RegExp ? 
 [[_prefix.exec(m.text), _prefix]] :
 Array.isArray(_prefix) ?
