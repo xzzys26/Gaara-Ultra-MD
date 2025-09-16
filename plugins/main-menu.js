85c92a7e089e22010a2d1f593391a69342d428d4
xzzys26 Creador De Gaara-Ultra-MD <erenxz01@gmail.com>
2025-09-16 09:32:04 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -79,7 +79,7 @@ ${Object.keys(tags).map(tag => {
   let section = `
 > ┃ ➪ ${tags[tag]}
 ${commandsForTag.map(menu => menu.help.map(help =>
-  `> ┃ ╰┈➤ ⚡︎ ${_p}${help}${menu.limit ? ' 🟡' : ''}${menu.premium ? ' 🔒' : ''}`
+  `> ╰┈➤ ⚡︎ ${_p}${help}${menu.limit ? ' 🟡' : ''}${menu.premium ? ' 🔒' : ''}`
 ).join('\n')).join('\n')}`
   return section
 }).filter(text => text !== '').join('\n')}
