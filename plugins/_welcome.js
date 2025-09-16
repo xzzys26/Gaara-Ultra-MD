bd8226e1dbd222a10390ed603fb3709a1a69f250
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-15 22:08:11 -0700

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,43 +1,61 @@
 //codigo creado por BrayanOFC 
 import { generateWAMessageFromContent, prepareWAMessageMedia } from '@whiskeysockets/baileys'
 
+const links = {
+  welcome: 'https://qu.ax/SfjSV.png',
+  bye: 'https://qu.ax/Rddry.png'
+}
+
 export async function welcomeHandler(conn, update) {
   try {
     const { id, participants, action } = update
     for (let user of participants) {
       if (action === 'add') {
-        let welImg = 'https://qu.ax/SfjSV.png'
-        let msg = await prepareWAMessageMedia({ image: { url: welImg } }, { upload: conn.waUploadToServer })
+        
+        let msg = await prepareWAMessageMedia(
+          { image: { url: links.welcome } },
+          { upload: conn.waUploadToServer }
+        )
 
-        const m = generateWAMessageFromContent(id, {
-          viewOnceMessage: {
-            message: {
-              ...msg,
-              caption: ` Bienvenido Bro @${user.split('@')[0]}`
+        const m = generateWAMessageFromContent(
+          id,
+          {
+            viewOnceMessage: {
+              message: {
+                ...msg,
+                caption: `👋 Bienvenido Bro @${user.split('@')[0]}`
+              }
             }
-          }
-        }, { userJid: conn.user.id })
+          },
+          { userJid: conn.user.id }
+        )
 
         await conn.relayMessage(id, m.message, { messageId: m.key.id })
 
       } else if (action === 'remove') {
-         let byeImg = 'https://qu.ax/Rddry.png'
-        let msg = await prepareWAMessageMedia({ image: { url: byeImg } }, { upload: conn.waUploadToServer })
+        
+        let msg = await prepareWAMessageMedia(
+          { image: { url: links.bye } },
+          { upload: conn.waUploadToServer }
+        )
 
-        const m = generateWAMessageFromContent(id, {
-          viewOnceMessage: {
-            message: {
-              ...msg,
-              caption: `Nadie te quiso 💀 @${user.split('@')[0]}  
-`
+        const m = generateWAMessageFromContent(
+          id,
+          {
+            viewOnceMessage: {
+              message: {
+                ...msg,
+                caption: `👋 Nadie te quiso 💀 @${user.split('@')[0]}`
+              }
             }
-          }
-        }, { userJid: conn.user.id })
+          },
+          { userJid: conn.user.id }
+        )
 
         await conn.relayMessage(id, m.message, { messageId: m.key.id })
       }
     }
   } catch (e) {
-    console.log(e)
+    console.log('❌ Error en welcomeHandler:', e)
   }
 }
