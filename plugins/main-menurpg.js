9a2afb2b88762c0f600830c7672dfdf9837ef6e9
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-15 22:19:02 -0700

    Actualizar main-menurpg.js

--- plugins/main-menurpg.js
+++ plugins/main-menurpg.js
@@ -2,8 +2,6 @@
 import { generateWAMessageFromContent, prepareWAMessageMedia } from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
-    const creador = ['5216641784469'];
-
     const creador = ['5216641784469']
 
 let handler = async (m, { conn, usedPrefix: _p }) => {
