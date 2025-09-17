c65b8f3462f212bcb8b5bcdbea35cc6cfaef1cf2
xzzys26 Creador De Gaara-Ultra-MD <erenxz01@gmail.com>
2025-09-17 18:15:31 -0400

    Actualizar economia-depositar.js

--- plugins/economia-depositar.js
+++ plugins/economia-depositar.js
@@ -3,27 +3,27 @@ import db from '../lib/database.js'
 let handler = async (m, { args }) => {
     let user = global.db.data.users[m.sender]
 
-    if (!args[0]) return m.reply(`💙 Ingresa la cantidad de *${moneda}* que deseas depositar.`)
-    if ((args[0]) < 1) return m.reply(`💫 Ingresa una cantidad válida de *${moneda}*.`)
+    if (!args[0]) return m.reply(`💸 Ingresa la cantidad de *${moneda}* que deseas depositar.`)
+    if ((args[0]) < 1) return m.reply(`💰 Ingresa una cantidad válida de *${moneda}*.`)
 
     if (args[0] === 'all') {
         let count = parseInt(user.coin)
         user.coin -= count
         user.bank += count
-        await m.reply(`💙 Depositaste *${count} ${moneda}* en el banco, ya no podrán robártelo.`)
+        await m.reply(`🏛 Depositaste *${count} ${moneda}* en el banco, ya no podrán robártelo.`)
         return
     }
 
-    if (!Number(args[0])) return m.reply(`💙 Debes depositar una cantidad válida.\n> Ejemplo 1 » *#d 25000*\n> Ejemplo 2 » *#d all*`)
+    if (!Number(args[0])) return m.reply(`💸 Debes depositar una cantidad válida.\n> Ejemplo 1 » *#d 25000*\n> Ejemplo 2 » *#d all*`)
 
     let count = parseInt(args[0])
-    if (!user.coin) return m.reply(`💫 No tienes suficientes *${moneda}* en la Cartera.`)
+    if (!user.coin) return m.reply(`❌️ No tienes suficientes *${moneda}* en la Cartera.`)
     if (user.coin < count) return m.reply(`${emoji2} Solo tienes *${user.coin} ${moneda}* en la Cartera.`)
 
     user.coin -= count
     user.bank += count
 
-    await m.reply(`💙 Depositaste *${count} ${moneda}* en el banco, ya no podrán robártelo.`)
+    await m.reply(`✅️ Depositaste *${count} ${moneda}* en el banco, ya no podrán robártelo.`)
 }
 
 handler.help = ['depositar']
