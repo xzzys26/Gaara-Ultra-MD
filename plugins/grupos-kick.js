35d6898f6123824f5069929e1bc1b5cee63f8156
xzzys26 Creador De Gaara-Ultra-MD <erenxz01@gmail.com>
2025-09-17 17:05:50 -0400

    Actualizar grupos-kick.js

--- plugins/grupos-kick.js
+++ plugins/grupos-kick.js
@@ -1,19 +1,19 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 
   if (!m.mentionedJid[0] && !m.quoted) 
-    return m.reply(`🐉 Ingresa el tag de un usuario. Ejemplo:\n\n*${usedPrefix + command}* @tag`) 
+    return m.reply(`⚡️ Ingresa el tag de un usuario. Ejemplo:\n\n*${usedPrefix + command}* @tag`) 
 
   let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 
   if (user === conn.user.jid) 
-    return m.reply(`☁️ No puedo hacer un auto kick`)
+    return m.reply(`❌️ No puedo hacer un auto kick`)
 
   let creadorJids = global.creador.map(([id]) => id + '@s.whatsapp.net')
   if (creadorJids.includes(user)) 
-    return m.reply(`⚡ No puedes expulsar al creador del bot`)
+    return m.reply(`🔱 No puedes expulsar al creador del bot`)
 
   await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-  m.reply(`🔮 Usuario eliminado con éxito`) 
+  m.reply(`✅️ Usuario eliminado con éxito`) 
 
 }
 
