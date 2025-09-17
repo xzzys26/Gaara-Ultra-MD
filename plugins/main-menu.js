c60ce1de61821dd8d15a1eca14c56d11f40a16a6
xzzys26 Creador De Gaara-Ultra-MD <erenxz01@gmail.com>
2025-09-17 17:03:10 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -115,7 +115,7 @@ ${comandos.map(menu => menu.help.map(help =>
     await m.react('🥷')
     await m.react('⚡')
 
-    let vidBuffer = await (await fetch('https://files.catbox.moe/l9g3ax.mp4')).buffer() // cambia la URL por tu video corto
+    let vidBuffer = await (await fetch('https://files.catbox.moe/z0ygkv.mp4')).buffer() // cambia la URL por tu video corto
     let media = await prepareWAMessageMedia(
       { video: vidBuffer, gifPlayback: true },
       { upload: conn.waUploadToServer }
