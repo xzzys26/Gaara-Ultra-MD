559610342bf70f9ae1d9acd355567f94ff6153ac
xzzys26 Creador De Gaara-Ultra-MD <erenxz01@gmail.com>
2025-09-17 19:19:08 -0400

    Actualizar grupos-salirgc.js

--- plugins/grupos-salirgc.js
+++ plugins/grupos-salirgc.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `🐉 *${botname}* Abandona El Grupo, Fué Genial Estar Aquí ☁️`) 
+await conn.reply(id, `🥷🏼 *${botname}* Abandona El Grupo, Fué Genial Estar Aquí ⚡️`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
