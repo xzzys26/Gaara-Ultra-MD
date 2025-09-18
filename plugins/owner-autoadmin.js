4b624b245eb219cede57fab0783bbc1acb644871
BrayanOFC2 <martinezalexis84280@gmail.com>
2025-09-17 18:05:57 -0700

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -2,8 +2,8 @@ const handler = async (m, {conn, isAdmin, groupMetadata }) => {
   if (isAdmin) return m.reply('👑 *Tu ya eres admin.*');
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
-  await m.react(done)
-   m.reply('👑 *Ya quedó.⚡️*');
+  await m.react(✔️)
+   m.reply('👑 *Ya quedó patron🥷.*');
   } catch {
     m.reply('⚠️ Ocurrio un error.');
   }
