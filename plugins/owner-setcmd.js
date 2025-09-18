60bfae8eca33326776ba61ce52c546eda1cd4087
BrayanOFC2 <martinezalexis84280@gmail.com>
2025-09-17 18:29:40 -0700

    Actualizar owner-setcmd.js

--- plugins/owner-setcmd.js
+++ plugins/owner-setcmd.js
@@ -1,24 +1,34 @@
-let handler = async (m, { text, usedPrefix, command }) => {
-global.db.data.sticker = global.db.data.sticker || {}
-if (!m.quoted) return conn.reply(m.chat, `🥷 Responda a un sticker para agregar un comando.`, m, rcanal)
-if (!m.quoted.fileSha256) return conn.reply(m.chat, `⚡️ Responda a un sticker para agregar un comando.`, m, rcanal)
-if (!text) return conn.reply(m.chat, `🚀 Ingresa el nombre del comamdo.`, m, rcanal)
-try {
-let sticker = global.db.data.sticker
-let hash = m.quoted.fileSha256.toString('base64')
-if (sticker[hash] && sticker[hash].locked) return conn.reply(m.chat, `❌️ No tienes permiso para cambiar este comando de Sticker.`, m, rcanal)
-sticker[hash] = {
-text,
-mentionedJid: m.mentionedJid,
-creator: m.sender,
-at: + new Date,
-locked: false,
+let handler = async (m, { conn, text }) => {
+  global.db.data.sticker = global.db.data.sticker || {}
+  
+  if (!m.quoted) return conn.reply(m.chat, `🥷 Responde a un *sticker* para agregar un comando.`, m)
+  if (!m.quoted.fileSha256) return conn.reply(m.chat, `⚡️ Ese sticker no tiene un *hash válido*.`, m)
+  if (!text) return conn.reply(m.chat, `🚀 Ingresa el nombre del comando.`, m)
+  
+  try {
+    let sticker = global.db.data.sticker
+    let hash = m.quoted.fileSha256.toString('base64')
+    
+    if (sticker[hash] && sticker[hash].locked) 
+      return conn.reply(m.chat, `❌️ No tienes permiso para cambiar este comando de Sticker.`, m)
+    
+    sticker[hash] = {
+      text,
+      mentionedJid: m.mentionedJid,
+      creator: m.sender,
+      at: + new Date,
+      locked: false,
+    }
+    
+    await conn.reply(m.chat, `⚡️ Comando guardado con éxito.`, m)
+    await m.react('✅')
+    
+  } catch (e) {
+    console.error(e) // 🔎 Para ver el error real en consola
+    await m.react('✖️')
+  }
 }
-await conn.reply(m.chat, `⚡️ Comando guardado con exito.`, m, rcanal)
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
+
 handler.help = ['cmd'].map(v => 'set' + v + ' *<texto>*')
 handler.tags = ['owner']
 handler.command = ['setcmd', 'addcmd', 'cmdadd', 'cmdset']
