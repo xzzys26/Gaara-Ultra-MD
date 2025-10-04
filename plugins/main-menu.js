4a67ea6846afe543c3e8875be7f91f9446c5312b
xzzys26 <erenxz01@gmail.com>
2025-10-03 20:04:24 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -100,7 +100,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       menuText += `
 ╭━━━〔 ${tags[tag]} 〕━━━⬣
 ${comandos.map(menu => menu.help.map(help =>
-  `┃ ⚡︎ ${_p}${help}${menu.limit ? ' 🟡' : ''}${menu.premium ? ' 🔒' : ''}`
+  `┃ ➟ ${_p}${help}${menu.limit ? ' 🟡' : ''}${menu.premium ? ' 🔒' : ''}`
 ).join('\n')).join('\n')}
 ╰━━━━━━━━━━━━━━━━━━━━━━⬣
 `
